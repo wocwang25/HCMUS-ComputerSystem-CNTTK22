27e14b81c1e9b94f257db9122f17fe78ae2a2bc9
Vo Quoc Quang <vqquangys25@gmail.com>
2024-03-25 23:33:13 +0700

    Chuyển đổi từ mã binary sang số chấm động

--- Cau02.cpp
+++ Cau02.cpp
@@ -3,27 +3,30 @@
 #include <cstring> 
 #include <cmath>   
 
-void forceFloat(float *p, char *s) {
-    if(strlen(s) != 32) {
+void forceFloat(float *p, const char *s) {
+    if(strlen(s) != 32) 
+    {
         std::cerr << "Error: Input string must be 32 bits long." << std::endl;
         return;
     }
-
-    unsigned int intValue = 0; 
-    for(int i = 0; i < 32; i++) {
-        if(s[i] == '1') {
-            intValue += 1 << (31 - i);
-        } else if(s[i] != '0') {
-            std::cerr << "Error: Input string must contain only '0's and '1's." << std::endl;
-            return;
-        }
+    int sign = (s[0] == '1') ? -1 : 1; 
+    int exponent = 0; 
+    float mantissa = 1.0f; 
+    for(int i = 1; i <= 8; i++) 
+    {
+        exponent += (s[i] - '0') * std::pow(2, 8 - i);
     }
-    *p = *reinterpret_cast<float*>(&intValue);
+    exponent -= 127;
+    for(int i = 9; i < 32; i++)
+     {
+        mantissa += (s[i] - '0') * std::pow(2, 8 - i);
+    }
+    *p = sign * mantissa * std::pow(2, exponent);
 }
 
 int main() {
     float myFloat;
-    char binaryString[] = "01000001001101100000000000000000";
+    char binaryString[] = "01000100001101100001000000000000";
 
     forceFloat(&myFloat, binaryString);
 
